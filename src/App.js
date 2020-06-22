f498bc683cc301f834aa098ad76cf8f856aea69a
Terry Chiem <terrychiem@gmail.com>
2020-06-22 16:24:06 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+/**
+ * Main app for REST Countries. Fetches all country data from API on mount.
+ */
+
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  useEffect(() => {
+    // load country data from api
+  }, []);
+
+  // if no dataLoaded, show loading spinner
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello, world.
     </div>
   );
 }
